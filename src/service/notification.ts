08ac9a0e0fdfa82a06aba62f5cc6b720a56b5dc3
Hauck <celiohauck@gmail.com>
2021-09-26 18:50:37 -0300

    feat(metrica): Implementação do push notification

--- src/service/notification.ts
+++ src/service/notification.ts
@@ -1,11 +1,48 @@
-export const sendNotification = async (token: string) => {
-  //   const hasSomeToken = await hasToken(token);
-  //   if (!hasSomeToken) {
-  //     const response = await httpClientToken.post({
-  //       action: '',
-  //       body: { value: token },
-  //     });
-  //     return response.data;
-  //   }
-  //   return;
+import admin = require('firebase-admin');
+import { getAllTokens } from './token';
+
+const serviceAccount = require('../../vazou-2d0bd-firebase-adminsdk-m846w-525d8684f6.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://vazou-2d0bd-default-rtdb.firebaseio.com',
+});
+
+const skeletonMessage = {
+  notification: {
+    title: 'Vazamento de gas!',
+    body: 'Detectamos um vazamento de gas critico na sua residencia!',
+  },
+  webpush: {
+    notification: {
+      icon: 'https://vazou-aabwqe8xv-celiohauck.vercel.app/logo192.png',
+    },
+    fcmOptions: {
+      link: 'https://vazou-aabwqe8xv-celiohauck.vercel.app/medidor',
+    },
+  },
+};
+
+const mountMessage = async () => {
+  const tokens = await getAllTokens();
+
+  const messages = tokens.map((token) => {
+    return { ...skeletonMessage, token: token.value };
+  });
+
+  return messages;
+};
+
+export const sendNotification = async () => {
+  const messages = await mountMessage();
+
+  admin
+    .messaging()
+    .sendAll(messages)
+    .then((response) => {
+      console.log(response.successCount + ' messages were sent successfully');
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
--- src/service/token.ts
+++ src/service/token.ts
@@ -8,11 +8,26 @@ export const saveToken = async (token: string) => {
   if (!hasSomeToken) {
     const response = await httpClientToken.post({
       action: '',
-      body: { value: token },
+      body: {
+        value: token,
+      },
     });
     return response.data;
   }
   return;
+};
+
+export const getAllTokens = async (): Promise<IToken[]> => {
+  const result = await httpClientToken.get({ action: '' });
+  if (result && result.data) {
+    const { data } = result;
+    const tokens = Object.keys(data).map<IToken>((key) => {
+      return { id: key, ...data[key] };
+    });
+    return tokens;
+  } else {
+    return [];
+  }
 };
 
 const hasToken = async (token: string) => {
